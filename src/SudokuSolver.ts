cddbfc79c05f09d749f63b0f6843d8809a3d00ba
Clément Baudet <10958405+cbdt@users.noreply.github.com>
2020-03-10 13:46:38 -0400

    change core principle
    Cannot do AC3 otherwise...

--- src/SudokuSolver.ts
+++ src/SudokuSolver.ts
@@ -1,11 +1,13 @@
 const BOX_SIZE = 3;
 const SIZE = BOX_SIZE * BOX_SIZE;
+const COLUMNS: string = "123456789";
+const ROWS: string = "ABCDEFGHI";
 
 class Sudoku {
   static sudokuSolver: SudokuSolver;
 
-  private static getBoardFromHtml(): Cell[][] {
-    let cells: Cell[][] = [];
+  private static getBoardFromHtml(): Map<string, number> {
+    let cells: Map<string, number> = new Map();
     const rootInput = document.querySelector("#sudokuInput");
     const inputs = rootInput.querySelectorAll("input[type=text]");
     inputs.forEach((input: HTMLInputElement) => {
@@ -13,20 +15,19 @@ class Sudoku {
       if (cells[x] === undefined) cells[x] = [];
       const y = input.getAttribute("data-y");
       const value = input.value;
-      let cell = new Cell(value === "" ? 0 : parseInt(value));
-      cells[x][y] = cell;
+      cells.set(y + "" + x, value === "" ? 0 : parseInt(value));
     });
     return cells;
   }
 
-  private static displayBoard(cells: Cell[][]): void {
+  private static displayBoard(cells: Map<string, number>): void {
     const rootInput = document.querySelector("#sudokuSolution");
     const inputs = rootInput.querySelectorAll("input[type=text]");
     inputs.forEach((input: HTMLInputElement) => {
       const x = input.getAttribute("data-x");
-      if (cells[x] === undefined) cells[x] = [];
       const y = input.getAttribute("data-y");
-      input.value = cells[x][y].value !== 0 ? cells[x][y].value : "";
+      input.value =
+        cells.get(y + "" + x) !== 0 ? cells.get(y + "" + x).toString() : "";
       input.disabled = true;
     });
   }
@@ -50,102 +51,105 @@ class Sudoku {
 
 class SudokuSolver {
   /** Cell[x][y] */
-  board: Cell[][];
+  board: Map<string, number>;
+  variables: Array<string>;
+  constraints: Array<Array<string>>;
+  binary_constraints: Array<Array<string>>;
+  neighbors: Map<string, Array<string>>;
+  possibilities: Map<string, Array<number>>;
 
-  constructor(board: Cell[][]) {
+  constructor(board: Map<string, number>) {
     this.board = board;
+    this.variables = this.cross(COLUMNS, ROWS);
+    this.neighbors = new Map();
+    this.possibilities = new Map();
+    this.constraints = new Array();
+    this.binary_constraints = new Array();
 
-    // On initialise les contraintes initiales.
-    for (let x = 0; x < SIZE; x++) {
-      for (let y = 0; y < SIZE; y++) {
-        this.updateConstraints(x, y);
+    // GENERATION DES POSSIBILITES
+
+    for (const [position, value] of this.board) {
+      this.possibilities.set(
+        position,
+        value === 0 ? [1, 2, 3, 4, 5, 6, 7, 8, 9] : [value]
+      );
+    }
+
+    // GENERATION DES CONTRAINTES
+
+    for (let i = 0; i < ROWS.length; i++) {
+      this.constraints.push(this.cross(ROWS[i], COLUMNS));
+      this.constraints.push(this.cross(ROWS, COLUMNS[i]));
+    }
+
+    for (const _bi of ["ABC", "DEF", "GHI"]) {
+      for (const _bj of ["123", "456", "789"]) {
+        this.constraints.push(this.cross(_bi, _bj));
+      }
+    }
+
+    // Transformation en contraintes binaires
+
+    for (const c of this.constraints) {
+      this.binary_constraints.push(...k_combinations<string>(c, 2));
+    }
+
+    this.binary_constraints = [...new Set(this.binary_constraints)]; // Enlève les doublons.
+
+    // GENERATION DES VOISINS
+
+    for (const v of this.variables) {
+      this.neighbors.set(v, new Array<string>());
+      for (const c of this.binary_constraints) {
+        if (v === c[0]) this.neighbors[v].push(c[1]);
       }
     }
   }
 
-  /**
-   * Ajoute des contraintes à la cellule (x; y).
-   * Correspond à enlever les valeurs possibles qui ont déjà été attribué sur la ligne / la colonne / le carré.
-   * @param x
-   * @param y
-   */
-  private updateConstraints(x: number, y: number): void {
-    const cell = this.board[x][y];
-    cell.addConstraints(this.lineConstraints(y));
-    cell.addConstraints(this.columnConstraints(x));
-    cell.addConstraints(this.boxConstraints(x, y));
-  }
-
-  private ac3(): void {}
-  private lcv(): void {}
-
-  private mrv(): void {}
-
-  private lineConstraints(row: number): Set<number> {
-    let constraints = new Set<number>();
-    for (let col = 0; col < SIZE; col++) {
-      let cell = this.board[col][row];
-      if (cell.value !== undefined) {
-        constraints.add(cell.value);
-      }
-    }
-    return constraints;
-  }
-  private columnConstraints(col: number): Set<number> {
-    let constraints = new Set<number>();
-    for (let row = 0; row < SIZE; row++) {
-      let cell = this.board[col][row];
-      if (cell.value !== undefined) {
-        constraints.add(cell.value);
-      }
-    }
-    return constraints;
-  }
-  private boxConstraints(col: number, row: number): Set<number> {
-    let constraints = new Set<number>();
-    let cornerX = col - (col % BOX_SIZE);
-    let cornerY = row - (row % BOX_SIZE);
-
-    for (let _col = cornerX; _col < cornerX + BOX_SIZE; _col++) {
-      for (let _row = cornerY; _row < cornerY + BOX_SIZE; _row++) {
-        let cell = this.board[col][row];
-        if (cell.value !== undefined) {
-          constraints.add(cell.value);
-        }
-      }
-    }
-    return constraints;
-  }
-
-  public process(): [boolean, Cell[][]] {
+  public process(): [boolean, Map<string, number>] {
     // TODO: To remove
-    const cells: Cell[][] = this.board; // for now we display the same cells to see if it works.
+    const cells: Map<string, number> = this.board; // for now we display the same cells to see if it works.
     const isResolved = true;
 
     return [isResolved, cells];
   }
-}
 
-class Cell {
-  ///position: [number, number];
-  value: number | undefined; // if undefined, no value (yet)
-  possible_values: Set<number> = new Set<number>([0, 1, 2, 3, 4, 5, 6, 7, 8]);
-
-  constructor(value: number) {
-    this.value = value;
-  }
-
-  addConstraints(values: Set<number>) {
-    this.removeAll(this.possible_values, values);
-  }
-
-  public isEmpty(): boolean {
-    return this.value === undefined;
-  }
-
-  private removeAll(originalSet: Set<number>, toBeRemovedSet: Set<number>) {
-    [...toBeRemovedSet].forEach(function(v) {
-      originalSet.delete(v);
-    });
+  private cross(A: string, B: string): Array<string> {
+    let res = new Array<string>();
+    for (let _a of A) {
+      for (let _b of B) {
+        res.push(_a + "" + _b);
+      }
+    }
+    return res;
   }
 }
+
+// UTILS
+
+// https://gist.github.com/axelpale/3118596
+const k_combinations = <T>(set: T[], k: number): T[][] => {
+  if (k > set.length || k <= 0) {
+    return [];
+  }
+
+  if (k === set.length) {
+    return [set];
+  }
+
+  if (k === 1) {
+    return set.reduce((acc, cur) => [...acc, [cur]], [] as T[][]);
+  }
+
+  const combs = [] as T[][];
+  let tail_combs = [];
+
+  for (let i = 0; i <= set.length - k + 1; i++) {
+    tail_combs = k_combinations(set.slice(i + 1), k - 1);
+    for (let j = 0; j < tail_combs.length; j++) {
+      combs.push([set[i], ...tail_combs[j]]);
+    }
+  }
+
+  return combs;
+};
